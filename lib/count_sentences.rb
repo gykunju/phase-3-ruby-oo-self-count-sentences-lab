f33360f7b14f7637d587224d571eed8705cf941b
Alvin <alvin.gikunju@student.moringaschool.com>
2023-07-07 23:14:59 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!.?]+/).count
   end
 end
+
+puts "Hello".sentence?
+puts "This is a string! It has three sentences. Right?".count_sentences
